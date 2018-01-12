2ec1cbd361c882afe2bc50ea60c8c111c3d104df
Bjorn <bjossijohnson@gmail.com>
2018-01-12 22:38:05 +0100

    Bjorn´s beginning

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -4,8 +4,37 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        Find me in src/app.js!
+      <div className="form-container">
+        <div className="tab">Flyg</div>
+        <form className="form">
+          <div className="upper-part">
+            <section>
+              <div className="autosuggest">
+                <label className="from">Flyg från</label>
+                <input type="text" className="inputCities" placeholder="Var flyger du ifrån?" />
+                {/* <button className="X">X</button> */}
+                <div className="shortcut-cities">
+                  <span>Stockolm</span>, <span>Göteborg</span>, eller <span>Malmö</span>
+                </div>
+              </div>
+            </section>
+            <section>
+              <div className="autosuggest">
+                <label className="from">Flyg till</label>
+                <input type="text" className="inputCities" placeholder="Vart vill du resa?" />
+                {/* <button className="X">X</button> */}
+              </div>
+            </section>
+          </div>
+          <div className="flight-details">
+            <hr className="line" />
+          </div>
+          <div className="lower-part">
+            <button className="enter">Sök och beställ</button>
+            eller <span>avbryt</span>
+          </div>
+        </form>
+
       </div>
     )
   }
